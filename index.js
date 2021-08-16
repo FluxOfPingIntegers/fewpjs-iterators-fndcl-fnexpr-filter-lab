a60c7808d12e2067f926e6b2f260f1e09ede57ec
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-16 12:01:24 -0400

    commit tree 728a830186d478f6a10a80c8a74d16afec329693

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, argString) {
+  return drivers.filter(driver => {return driver.toUpperCase() === argString.toUpperCase()})
+}
+
+function fuzzyMatch(drivers, nameStr) {
+  return drivers.filter(driver => {return driver.startsWith(nameStr)})
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => {return driver.name === name} );
+}
